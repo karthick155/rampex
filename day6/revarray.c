207de372de48abeca55c70cf163574890e0b776b
karthick155 <karthickthiyagu2002@gmail.com>
2025-05-17 15:30:07 +0530

    day6

---
+++ day6/revarray.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+}
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, n);
+
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
